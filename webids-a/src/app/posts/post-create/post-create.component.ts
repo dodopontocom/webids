91b050ce19c6191bbcd88f70c8b633298b1b961b
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-18 17:42:15 +0000

    go post

--- webids-a/src/app/posts/post-create/post-create.component.ts
+++ webids-a/src/app/posts/post-create/post-create.component.ts
@@ -5,7 +5,10 @@ import { Component } from '@angular/core';
   templateUrl:'./post-create.component.html'
 })
 export class PostCreateComponent {
-  onSavePost() {
-    alert("olá")
+
+  newPost = '';
+
+  onAddPost() {
+    this.newPost = "The user\'s Post";
   }
 }
