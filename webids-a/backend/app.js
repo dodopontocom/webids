29568a6e87cc619bf07fd4bde82ca0cb7500b594
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-23 11:29:04 -0300

    Update app.js

--- webids-a/backend/app.js
+++ webids-a/backend/app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers",
     "Origin, X-Request-With",
-    "Content-Type, Accept"
+    "Content-Type, Accept, application/x-www-form-urlencoded, charset=UTF-8, Authorization, X-Requested-With"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
