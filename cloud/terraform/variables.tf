db1979e69cff00c99d50959dd2f0bca5fac60fff
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-24 09:39:08 -0300

    Create variables.tf

---
+++ cloud/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" { }
+variable "region" { }
+variable "zone" { }
+variable "key" { }
+variable "gcp_bucket" { }
+variable "machine_type" { }
+variable "cluster_count" { }
+variable "cluster_name" { }
+variable "ssd_name" { }
+variable "compute_instance_environment" { }
+variable "ubuntu_image" { }
+variable "startup_script" { }
+
+variable "MONGO_ATLAS_STRING" { }
+variable "JWT_KEY" { }
