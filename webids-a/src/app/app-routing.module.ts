e51970b7fb15ad0c9c430e91ac775366e479e1a2
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-24 10:44:13 -0300

    Create app-routing.module.ts

---
+++ webids-a/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '' }
+  }
+];
+
+@NgModule({
+
+})
+export class AppRoutingModule {
+
+}
