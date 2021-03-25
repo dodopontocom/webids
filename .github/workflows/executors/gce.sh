8faaae9495e250f2597f1f57eb69c8ecf070bf69
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-25 20:50:41 -0300

    Update gce.sh

--- .github/workflows/executors/gce.sh
+++ .github/workflows/executors/gce.sh
@@ -3,7 +3,9 @@
 source ${GITHUB_WORKSPACE}/.github/workflows/cicd-definitions.sh
 terraform_path="${GITHUB_WORKSPACE}/cloud/terraform"
 
+gsutil iam get gs://${GCLOUD_APP_BUCKET_NAME}
+
 cd ${terraform_path}
-terraform init --backend-config="bucket=umaviao" --backend-config="prefix=tf-state"
+terraform init --backend-config="bucket=${GCLOUD_APP_BUCKET_NAME}" --backend-config="prefix=tf-state"
 terraform plan
 #terraform apply --auto-approve
