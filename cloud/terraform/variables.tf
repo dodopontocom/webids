b3a5256457349ff3634c87b0ea3e511732611610
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-25 11:11:58 -0300

    Update variables.tf

--- cloud/terraform/variables.tf
+++ cloud/terraform/variables.tf
@@ -2,11 +2,9 @@ variable "project_id" { }
 variable "region" { }
 variable "zone" { }
 variable "key" { }
-variable "gcp_bucket" { }
 variable "machine_type" { }
 variable "cluster_count" { }
 variable "cluster_name" { }
 variable "ssd_name" { }
 variable "compute_instance_environment" { }
 variable "ubuntu_image" { }
-variable "startup_script" { }
