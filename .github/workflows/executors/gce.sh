bc6d50fa227c1a07e635da7a919c0131516d8d89
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-25 11:35:18 -0300

    Update gce.sh

--- .github/workflows/executors/gce.sh
+++ .github/workflows/executors/gce.sh
@@ -3,12 +3,6 @@
 source ${GITHUB_WORKSPACE}/.github/workflows/cicd-definitions.sh
 terraform_path="${GITHUB_WORKSPACE}/cloud/terraform"
 
-echo 1
-echo ${service_account_key}
-echo 2
-echo ${secrets.WEBIDS_GCP_SA}
-#echo ${service_account_key} > ${TF_VAR_key}
-
-#cd ${terraform_path}
-#terraform init
-#terraform plan
+cd ${terraform_path}
+terraform init
+terraform plan
