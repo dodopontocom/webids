bcf39e8c1e710b0d67dae58850ba6736caa1a34b
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-25 11:14:34 -0300

    init

--- .github/workflows/executors/gce.sh
+++ .github/workflows/executors/gce.sh
@@ -3,5 +3,8 @@
 source ${GITHUB_WORKSPACE}/.github/workflows/cicd-definitions.sh
 terraform_path="${GITHUB_WORKSPACE}/cloud/terraform"
 
+echo ${service_account_key}
+
 cd ${terraform_path}
+terraform init
 terraform plan
