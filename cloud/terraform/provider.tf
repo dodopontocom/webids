8cbb623434ce8621092bc3beaeea521e8b1deeb4
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-25 13:06:11 -0300

    Update provider.tf

--- cloud/terraform/provider.tf
+++ cloud/terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.7"
 }
 provider "google" {
-  credentials = file(var.key)
+  //credentials = file(var.key)
   project     = var.project_id
   region      = var.region
 }
