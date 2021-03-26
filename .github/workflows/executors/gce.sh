3c0094046314c6c8367cac2f6167cd791f1028c9
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-26 10:32:41 -0300

    Update gce.sh

--- .github/workflows/executors/gce.sh
+++ .github/workflows/executors/gce.sh
@@ -6,13 +6,14 @@ echo "${WEBIDS_GCP_SA}" > ${GCLOUD_JSON_KEY_PATH}
 terraform_path="${GITHUB_WORKSPACE}/cloud/terraform"
 hasBucket=false
 
+echo ${hasBucket}
 gsutil ls gs://${GCLOUD_APP_BUCKET_NAME}
 if [[ "$?" -eq "0" ]]; then
     echo ok
 else
     gsutil mb -l ${GCLOUD_PROJECT_REGION} -p ${PROJECT_ID} -c standard gs://${GCLOUD_APP_BUCKET_NAME}
 fi
-
+echo ${hasBucket}
 if [[ "${hasBucket}" -eq "true" ]]; then
     cd ${terraform_path}
     terraform init --backend-config="bucket=${GCLOUD_APP_BUCKET_NAME}" --backend-config="prefix=tf-state"
