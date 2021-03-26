259b780d34aa9a3651c6f577896e8b7978be2935
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-26 13:12:22 -0300

    Update setup-env.sh

--- cloud/scripts/setup-env.sh
+++ cloud/scripts/setup-env.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
-
-apt-get install -y nodejs git npm
-npm install -g @angular/cli
+apt-get install -y nodejs npm
 
 git clone https://github.com/dodopontocom/webids.git
 cd webids/webids-a
 git checkout wip-gcp
+
+npm install -g @angular/cli
+npm install -g npm@6
+
 npm install
 ng serve --host 0.0.0.0 --port 4200
