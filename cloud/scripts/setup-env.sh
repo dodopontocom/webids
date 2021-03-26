6b751390098e50ce0a92868df5b35f9524bfbae8
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-26 10:50:51 -0300

    Create setup-env.sh

---
+++ cloud/scripts/setup-env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get install -y nodejs git npm
+npm install -g @angular/cli
+
+git clone https://github.com/dodopontocom/webids.git
+cd webids/webids-a
+git checkout wip-gcp
+npm install
+ng serve --host 0.0.0.0 --port 4200
