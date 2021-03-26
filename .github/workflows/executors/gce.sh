3035281812daebf0e7b23de5517342f2c1050f87
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-26 09:34:13 -0300

    Update gce.sh

--- .github/workflows/executors/gce.sh
+++ .github/workflows/executors/gce.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 source ${GITHUB_WORKSPACE}/.github/workflows/cicd-definitions.sh
+echo "${WEBIDS_GCP_SA}" > ${GCLOUD_JSON_KEY_PATH}
+
 terraform_path="${GITHUB_WORKSPACE}/cloud/terraform"
+hasBucket=false
 
-gsutil iam get gs://${GCLOUD_APP_BUCKET_NAME}
+[[ $(gsutil iam get gs://${GCLOUD_APP_BUCKET_NAME}) ]] && hasBucket=true
 
-cd ${terraform_path}
-terraform init --backend-config="bucket=${GCLOUD_APP_BUCKET_NAME}" --backend-config="prefix=tf-state"
-terraform plan
-#terraform apply --auto-approve
+if [[ "${hasBucket}" -eq "true" ]]; then
+    cd ${terraform_path}
+    terraform init --backend-config="bucket=${GCLOUD_APP_BUCKET_NAME}" --backend-config="prefix=tf-state"
+    terraform plan
+    #terraform apply --auto-approve
+fi
