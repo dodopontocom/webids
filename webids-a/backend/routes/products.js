4304bb6b7f7a9364083b2a99bbf39887f2c7d3d5
Rodolfo Neto <rodolfotiago@gmail.com>
2021-03-29 22:44:50 +0000

    file upload

--- webids-a/backend/routes/products.js
+++ webids-a/backend/routes/products.js
@@ -11,7 +11,7 @@ const MIME_TYPE_MAP = {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP(file.mimetype);
+    const isValid = MIME_TYPE_MAP[file.mimetype];
     let error = new Error("Invalid mime type");
     if (isvalid) {
       error = null;
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP(file.mimetype);
+    const ext = MIME_TYPE_MAP[file.mimetype];
     cb(null, name + '-' + Date.now() + '.' + ext);
   }
 });
