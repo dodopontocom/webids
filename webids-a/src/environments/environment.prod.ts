c8b3422cd8c821a277cd228ac90862718bb81f36
Rodolfo Neto <rodolfotiago@gmail.com>
2021-04-09 00:01:36 -0300

    Update environment.prod.ts

--- webids-a/src/environments/environment.prod.ts
+++ webids-a/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_TESTING_HOST: "https://backend-dot-continual-voice-276914.uc.r.appspot.com/api/v1"
+  API_TESTING_HOST: "https://backend-dot-webids.uc.r.appspot.com/api/v1"
 };
