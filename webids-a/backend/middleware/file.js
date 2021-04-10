ab12c4a2fe87770b43a913a4122f9a703d26f525
Neto <rodolfotiago@gmail.com>
2021-04-09 22:36:29 -0300

    multer google

--- webids-a/backend/middleware/file.js
+++ webids-a/backend/middleware/file.js
@@ -41,6 +41,11 @@ const uploadHandler = multer({
     cb(null, name + '-' + Date.now() + '.' + ext);
   },
   storage: multerGoogleStorage.storageEngine({
+    getFilename(req, file, cb) {
+      const name = file.originalname.toLowerCase().split(' ').join('-');
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, name + '-' + Date.now() + '.' + ext);
+    },
     acl: "publicread"
   })
 });
